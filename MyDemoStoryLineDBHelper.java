1e7c25b5639ff0b570ac9b41d9e0590077491848
brentghekiere <brent.ghekiere@student.howest.be>
2017-04-05 15:30:41 +0200

    Activities

---
+++ MyDemoStoryLineDBHelper.java
@@ -0,0 +1,47 @@
+package busit.busitgroup3;
+
+import cz.mendelu.busItWeek.library.StoryLineDatabaseHelper;
+import cz.mendelu.busItWeek.library.builder.StoryLineBuilder;
+
+public class MyDemoStoryLineDBHelper extends StoryLineDatabaseHelper {
+    public MyDemoStoryLineDBHelper() {
+        super(10);
+    }
+
+    @Override
+    protected void onCreate(StoryLineBuilder builder) {
+      /*  builder.addGPSTask("second one")
+                .victoryPoints(15)
+                .location(10, 10)
+                .radius(30)
+                .imageSelectPuzzle()
+                .puzzleTime(50000000)
+                .question("Choose an image")
+                .addImage(R.drawable.a, true)
+                .addImage(R.drawable.b, false)
+                .addImage(R.drawable.c, false)
+                .puzzleDone()
+                .taskDone();
+
+      builder.addBeaconTask("beacon")
+              .beacon(1,9)
+              .victoryPoints(15)
+              .location(49.209611, 16.614091)
+              .simplePuzzle()
+              .question("What is the meaning of life?")
+              .answer("BusIT Week")
+              .puzzleTime(454354354)
+              .puzzleDone().taskDone();
+               */
+
+      builder.addCodeTask("Code")
+              .location(49.209611, 16.614091)
+              .victoryPoints(13)
+              .qr("mendel")
+              .simplePuzzle()
+              .question("What is the best town in the world?")
+              .answer("Pittem")
+              .puzzleTime(456448564)
+              .puzzleDone().taskDone();
+    }
+}
---
+++ SimplePuzzleActivity.java
@@ -0,0 +1,68 @@
+package busit.busitgroup3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import cz.mendelu.busItWeek.library.SimplePuzzle;
+import cz.mendelu.busItWeek.library.StoryLine;
+import cz.mendelu.busItWeek.library.Task;
+
+public class SimplePuzzleActivity extends AppCompatActivity {
+
+    // 1. Get widgets references
+    private EditText answerTextView;
+    private TextView questionView;
+
+    // 2. Open storyline
+    private StoryLine storyLine;
+
+    private SimplePuzzle puzzle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_puzzle2);
+        // 1. Get widgets references
+        answerTextView = (EditText) findViewById(R.id.answerEditText);
+        questionView = (TextView) findViewById(R.id.questionTextView);
+        // 2. Open storyline
+        storyLine = StoryLine.open(this, MyDemoStoryLineDBHelper.class);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Task currentTask = storyLine.currentTask();
+        currentTask.getPuzzle();
+    /*
+        OPTIE A
+        Puzzle puzzle = currentTask.getPuzzle();
+
+        if (puzzle instanceof SimplePuzzle){
+           throw new IllegalStateException("No simple puzzle activity");
+
+        }
+        OPTIE B
+        */
+        puzzle = (SimplePuzzle) currentTask.getPuzzle();
+        questionView.setText(puzzle.getQuestion());
+    }
+
+    public void checkAnswer(View view) {
+        // 1. Get user's answer
+        String answer = answerTextView.getText().toString();
+
+        // 2. Get correct answer
+        String correctAnswer = puzzle.getAnswer();
+
+        if (answer.equalsIgnoreCase(correctAnswer)) {
+            storyLine.currentTask().finish(true);
+            finish();
+        } else {
+            answerTextView.setError(getString(R.string.error_no_bluetooth_enabled));
+        }
+    }
+}
