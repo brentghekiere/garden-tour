621e875ce01e7542fc8ef7c99b36f2e6a427ed70
DESKTOP-GMI5NRK\admin <pcendelinova@seznam.cz>
2017-04-06 09:24:41 +0200

    no message

--- BusITWeek/app/src/main/java/cz/mendelu/busItWeek/MainActivity.java
+++ BusITWeek/app/src/main/java/cz/mendelu/busItWeek/MainActivity.java
@@ -1,14 +1,11 @@
 package cz.mendelu.busItWeek;
 
-import android.*;
-import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageStats;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -16,8 +13,6 @@ import android.widget.TextView;
 
 import cz.mendelu.busItWeek.library.StoryLine;
 import cz.mendelu.busItWeek.library.Task;
-
-import cz.mendelu.busItWeek.R;
 
 public class MainActivity extends AppCompatActivity {
 
